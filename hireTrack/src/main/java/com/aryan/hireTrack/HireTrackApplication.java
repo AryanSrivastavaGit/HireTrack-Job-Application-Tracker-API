129f38d3624c94680376cfb85c399154f5bc312d
AryanSrivastavaGit <aryansrivastavag00916@gmail.com>
2026-06-17 20:17:15 +0530

    created hireTrack maven project, checked health

---
+++ hireTrack/src/main/java/com/aryan/hireTrack/HireTrackApplication.java
@@ -0,0 +1,13 @@
+package com.aryan.hireTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireTrackApplication.class, args);
+	}
+
+}
---
+++ hireTrack/src/test/java/com/aryan/hireTrack/HireTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryan.hireTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
